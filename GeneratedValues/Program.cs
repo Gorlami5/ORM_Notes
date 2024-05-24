5d3e270c50259b7dd9d00b0295d0039e53347280
Gorlami5 <muhammetyonkuc99@gmail.com>
2024-05-24 11:00:06 +0300

    generated values

--- GeneratedValues/Program.cs
+++ GeneratedValues/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Generated valuelerin genel hedefi verilerin uygulama dışında default değerler elde edilmesidir.Bu değerlerle birlikte Identity değerlerini de manipule edebiliriz.
+using System.ComponentModel.DataAnnotations.Schema;
+
+class Person
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)] // Burda iradeli olarak Pk özelliğini kaldırıyoruz.Eğer ki kaldırmazsak migrate ederken hata alacağız.
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Artık OtherId bir PK değeri olacaktır.Fakat ilk id değerinden PK özelliğini almamız lazm.
+    public int OtherId { get; set; }
+}
+//PK değerini başka bir kolon yapmak istediğimizde bunu bir attribute ile yapabiliriz.Fakat asıl olan PK değerini kaldırmak zorundayız.
+//PK değerini sayısal olmayan bir değere atamak istediğimizde PK değerini iradeli bir şekilde kaldırmaya ihtiyaç duymayız.
