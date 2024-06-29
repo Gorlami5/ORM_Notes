5e44b7c95f6699d026b78c2fc63c3bd57aac3455
Gorlami5 <muhammetyonkuc99@gmail.com>
2024-06-30 02:13:32 +0300

    Query Logs

--- Logging/Program.cs
+++ Logging/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using System.Runtime.InteropServices.Marshalling;
@@ -24,6 +25,10 @@ var query = context.Orders.ToList();
 //Son olarak loglama işlemi gelişmiş sistemlerde her servis için her bilgi tutmanın çok zor olduğunu düşünürsek bunun önüne geçebilmek için LogLevellar vardır.Bunları konfigürasyon dosyasından ayaralarız.
 //
 #endregion
+#region QueryLog
+//Lınq sorgularını teknik anlamda incelemek amacıyla kullanılan loglardır.Querylerdeki logları rahatlıkla hata ayıklama yapmamıza da yarar.
+//Query log için Microsoft.Extension.Logging.Console paketini projemize eklememiz gerekyor.Ardından OnConfiguring içerisinde ayarlamaları yaparız.
+#endregion
 class Post
 {
     public int Id { get; set; }
@@ -39,17 +44,18 @@ class Blog
 }
 class ConnectionDb2 : DbContext
 {
+    StreamWriter _log = new("logs.txt"); // dosya oluşturma
+    readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=OrmDb;Username=postgres;password=mukavina123;");
         /*optionsBuilder.LogTo(Console.WriteLine); */// Konsol örneği.istersek debug penceresine de yazdırabiliriz.
-        optionsBuilder.LogTo(message => _log.WriteLine(message),Microsoft.Extensions.Logging.LogLevel.Warning).EnableSensitiveDataLogging().EnableDetailedErrors(); // Bu dosya içerisinde tutma
-        //LogLeveli warning setleyerek artık logları sadece hata durumunda kayıt edilmesini sağlarız.
-
+        /* optionsBuilder.LogTo(message => _log.WriteLine(message),Microsoft.Extensions.Logging.LogLevel.Warning).EnableSensitiveDataLogging().EnableDetailedErrors();*/ // Bu dosya içerisinde tutma
+                                                                                                                                                                         //LogLeveli warning setleyerek artık logları sadece hata durumunda kayıt edilmesini sağlarız.
+        optionsBuilder.UseLoggerFactory(_loggerFactory); // Bu ayarlamadan sonra artık her query sonrası log yapacaktır.
     }
     
-    StreamWriter _log = new("logs.txt"); // dosya oluşturma
-
+   
     public DbSet<Blog> Orders { get; set; }
     public DbSet<Post> Posts { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
